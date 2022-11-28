92788c206011d13a4e4c51fb24d4ecb23c00f690
Percy <edoumps@gmail.com>
2022-11-28 13:38:02 +0100

    customer and booking models created. Updated workshop(remaining_sits attribut)

---
+++ app/models/customer.rb
@@ -0,0 +1,8 @@
+class Customer < ApplicationRecord
+
+  has_many :bookings
+  has_many :workshops, through: :bookings
+
+  validates :full_name, :contact_number, presence: true
+  validates :email, uniqueness: true, presence: true
+end
---
+++ db/migrate/20221128120216_rename_no_if_tickets_to_no_of_tickets.rb
@@ -0,0 +1,5 @@
+class RenameNoIfTicketsToNoOfTickets < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :bookings, :no_if_tickets, :no_of_tickets
+  end
+end
